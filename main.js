60b26981edcdcd7de7ab63b23302fd182f80baf4
Narmin-Osmanli <nermin.1599@gmail.com>
2024-10-27 21:51:21 +0400

    task

---
+++ main.js
@@ -0,0 +1,146 @@
+// 1.Task: Yazı(string) uzunluğunu tapın.
+
+// let string = 'Salam Dünya'
+
+// let length = string.length
+
+// console.log(string.length)
+
+// ----------------------------------------
+
+// 2.Task: Yazını böyük hərflərlə göstərin.
+
+// let text = 'azərbaycan'
+
+// let newtext = text.toUpperCase()
+
+// console.log(newtext) 
+
+// -----------------------------------------------
+
+// 3.Task: Array-de müəyyən bir hissəni kəsin.
+
+// let num = [ 1,2,3,4,5]
+
+// let num2= num.slice(2,4)
+
+// console.log(num2)
+
+// ==============================================
+
+// 4.Task: Arrayin sonuna element əlavə edin.
+
+// let num = [1,2,3]
+
+// num.push(4)
+
+// console.log(num)
+
+// ==============================================
+
+// 5.Task: Arrayin  sonundakı elementi silin.
+
+// let num = [1,2,3]
+// num.pop(3)
+
+// console.log(num)
+
+// ==============================================
+
+// 6.Task: Arrayin  əvvəlindən elementi silin.
+
+// let num = [1,2,3]
+
+// num.shift(1)
+// console.log(num)
+
+// ==============================================
+
+// 7.Task: Arrayin əvvəlinə element əlavə edin.
+
+// let num = [2,3]
+
+// num.unshift(1)
+
+// console.log(num)
+
+// ==============================================
+
+// 8.Task:  Arrayin  müəyyən bir elementi tapın.
+
+// let num = [5, 12, 8, 130, 44]
+
+// let num2 = num.find((array) => array > 10)
+
+// console.log(num2)
+
+// ==============================================
+
+// 9.Task: Array-dəki elementləri birləşdirin.
+
+// let num = [1,2,3,4];
+
+// let sum = num.reduce((acc,val) => (acc + val),
+// 0);
+
+// console.log(sum)
+
+// ==============================================
+
+// 10.Task: Array-i sıralayın.
+
+// let num = [3,4,1,2]
+
+// num.sort()
+
+// console.log(num)
+
+// ==============================================
+
+// 11. Task: Array-də müəyyən bir dəyərin olub olmadığını yoxlayın.
+
+// let num = [1,2,3]
+
+// console.log(num.includes(2))
+
+
+// ==============================================
+
+// 12.Task: Stringin müəyyən bir hissəsini yoxlayın.
+
+// let string = 'Salam Dünya'
+
+// console.log(string.includes('Dünya'))  
+
+// ==============================================
+
+// 13.Task: Array elementlərini birləşdirin.
+
+// let string = ['Salam','Dünya']
+
+// let sum = string.join(" ")
+
+// console.log(sum)
+
+
+
+// ==============================================
+
+// 14.Task: Arrayda filtr tətbiq edin.
+
+// let num = [1, 2, 3, 4, 5, 6, 13, 1]
+
+
+// let array = num.filter((num) => num % 2 == 0)  
+
+// console.log(array)
+
+// ==============================================
+
+// 15. Task:  array-dəki elementləri dəyişdirin.
+
+// let array = [1,2,3]
+
+// let result = array.map((num) => num * 2)
+
+// console.log(result)
